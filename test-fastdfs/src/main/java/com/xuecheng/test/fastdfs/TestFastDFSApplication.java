03d714d14202785ffa4ced0bcde285fc71e70d91
rukihuang <973157930@qq.com>
2020-02-08 19:02:37 +0800

    FastDFS测试工程创建

---
+++ test-fastdfs/src/main/java/com/xuecheng/test/fastdfs/TestFastDFSApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng.test.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hh
+ * @create 2020/2/8
+ * @description
+ **/
+@SpringBootApplication
+public class TestFastDFSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestFastDFSApplication.class);
+    }
+}
---
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -0,0 +1,20 @@
+package com.xuecheng.test.fastdfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author hh
+ * @create 2020/2/8
+ * @description
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestFastDFS {
+    //上传测试
+
+    //下载测试
+
+}
