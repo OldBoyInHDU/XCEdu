601fc4fa34b5f33198119742331cdd434b6c42dd
rukihuang <973157930@qq.com>
2020-02-08 14:05:43 +0800

    新增pageHelper

--- xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TestDao.java
+++ xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TestDao.java
@@ -1,5 +1,7 @@
 package com.xuecheng.manage_course.dao;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.xuecheng.framework.domain.course.CourseBase;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sound.midi.Soundbank;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -45,5 +48,14 @@ public class TestDao {
     public void testTeachplanMapper() {
         TeachplanNode teachplanNode = teachplanMapper.selectList("4028e581617f945f01617f9dabc40000");
         System.out.println(teachplanNode);
+    }
+
+    @Test
+    public void testPageHelper() {
+        //查询第一页，每页显示10条
+        PageHelper.startPage(1, 10);
+        Page<CourseBase> courseList = courseMapper.findCourseList();
+        List<CourseBase> result = courseList.getResult();
+        System.out.println(result);
     }
 }
