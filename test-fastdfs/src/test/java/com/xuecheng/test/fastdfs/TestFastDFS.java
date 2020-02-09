ca41dc32624442a403a660a2e893494c0406287e
rukihuang <973157930@qq.com>
2020-02-09 14:29:23 +0800

    FastDFS上传文件代码

--- test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -1,9 +1,13 @@
 package com.xuecheng.test.fastdfs;
 
+import org.csource.common.MyException;
+import org.csource.fastdfs.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 /**
  * @author hh
@@ -14,7 +18,41 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class TestFastDFS {
     //上传测试
+    @Test
+    public void testUpload() {
+        //加载fasddfs-client.properties配置文件
+        try {
+            ClientGlobal.initByProperties("config/fastdfs-client.properties");
+            //定义TrackClient，用于请求TrackerServer
+            TrackerClient trackerClient = new TrackerClient();
+            //连接tracker
+            TrackerServer trackerServer = trackerClient.getConnection();
+            //获取storage
+            StorageServer storeStorageServer = trackerClient.getStoreStorage(trackerServer);
+            //创建storageClient 通过tracker server 和 storage server 获得storageClient
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storeStorageServer);
+
+
+            //向storage服务器上传文件
+            //本地文件的路径
+            String filePath = "D:/prac_project/fdfs_test_upload.jpg";
+            //上传成功后拿到文件id
+            String fileId = storageClient1.upload_file1(filePath, "png", null);
+            System.out.println(fileId);//group1/M00/00/00/wKgAZ14_pb6ASZHeAAKBN9Um430128.png
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     //下载测试
+    public void testDownload() {
+
+    }
 
 }
