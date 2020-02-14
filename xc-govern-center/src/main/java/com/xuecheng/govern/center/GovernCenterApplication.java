8e898b17bf26f2fcb8408e9bf3806f82eb7a0804
rukihuang <973157930@qq.com>
2020-02-14 21:59:47 +0800

    day09:EurekaServer单机环境搭建

---
+++ xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng.govern.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hh
+ * @create 2020/2/14
+ * @description
+ **/
+@EnableEurekaServer //表示此工程是一个EurekaServer
+@SpringBootApplication
+public class GovernCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GovernCenterApplication.class);
+    }
+}
+
