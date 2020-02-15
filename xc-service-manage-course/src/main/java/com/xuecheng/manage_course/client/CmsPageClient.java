1ae79456f96dc1705962aa1717cb8a09a970379a
rukihuang <973157930@qq.com>
2020-02-15 19:36:50 +0800

    Feign负载均衡测试(Feign继承了ribbon)

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/client/CmsPageClient.java
@@ -0,0 +1,18 @@
+package com.xuecheng.manage_course.client;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author hh
+ * @create 2020/2/15
+ * @description
+ **/
+@FeignClient(value = "XC-SERVICE-MANAGE-CMS") //指定远程调用的服务名
+public interface CmsPageClient {
+    //根据页面Id查询页面信息，远程调用cms请求数据
+    @GetMapping("/cms/page/get/{id}") //用GetMapping标识远程调用的http的方法类型
+    public CmsPage findCmsPageById(@PathVariable("id") String id);
+}
